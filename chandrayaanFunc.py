d1d753d12a3306e50f3514fadefb0a0aa39e883f
Swastik Swarup Das <swastikswarupdas@gmail.com>
2023-08-16 14:41:44 +0530

    Major Comments Commit # 1

--- chandrayaanFunc.py
+++ chandrayaanFunc.py
@@ -1,9 +1,23 @@
+#The chandrayaanFunc is the function I've chosen for solving the problem statement. 
+#Following the rules of TDD, 
+#a. Test cases were made to be failed, code was refactored to pass only those test cases
+#b. Test cases were updated for the unit tests to fail again
+#c. Code was refactored again to pass those tests
+#d. Finally the required standard and quality was reached and a huge test case check passed
+
+
 def chandrayaanFunc(inPos,inDir,commands):
+    
     i = 0
     x, y, z = inPos
+    
+    #Initially, this variable wasnt a part, but it had to be made a part because While switchin from the up and down directions, the "belly facing" direction mattered a lot, we will get to that part
+
     prevDir=inDir
 
     while i < len(commands):
+
+    #The basic axis updations during the forward and backward movements 
 
         if commands[i] in ('f', 'b'):
             
@@ -46,10 +60,15 @@ def chandrayaanFunc(inPos,inDir,commands):
                 else:
                     y += 1
            
+    #Rotation Command updations
 
-        elif commands[i] in ('r','l'):
+        elif commands[i] in ('r','l'): # Left Right Rotation commands
+
+    # Other than when it is in Up or Down Position, we note where it was faced before that change
             if inDir in ('N','E','W','S'):
-                prevDir = inDir  # Left Right Rotation commands
+                prevDir = inDir  # this later becomes the "Belly Facing" direction
+
+    #Basic Clockwise rotations
             if inDir == "N":
                 if commands[i] == "r":
                     inDir = "E"
@@ -78,6 +97,10 @@ def chandrayaanFunc(inPos,inDir,commands):
                 else:
                     inDir = "S"
           
+    #This is where the Previous Direction variable prevDir comes into play 
+    
+    #So The belly direction is flipped everytime it goes from Up or Down or vice versa
+
             elif inDir == "U":
                 if commands[i] == "r":
                     if prevDir == "N":
