2ce5ce926a623d29d9960e1f279d99910a5582ca
Swastik Swarup Das <swastikswarupdas@gmail.com>
2023-08-16 14:27:20 +0530

    Test File Comments Commit # 1

--- test_chandrayaan.py
+++ test_chandrayaan.py
@@ -5,14 +5,16 @@ import unittest   # The test framework
 class Test_chandrayaan(unittest.TestCase):
     def test_chandrayaan(self):
         
-        inPos = [0,0,0]
-        inDir = "N"
+        inPos = [0,0,0] # Starting from Origin
+        inDir = "N" # Initially faces North 
         commands = ['f', 'r', 'u', 'd', 'u','f','f','r','r','f','f','l','d','f','r','f','r','u']
         
         expected_direction = "U"
         expected_pos = [-2, 1, 0]
         
         returned_position, returned_direction = chandrayaanFunc(inPos,inDir,commands)
+
+        #Although I believe two assertions are usually Bad practice according to Uncle Bob, I couldnt figure out how to do it inside one argument in the given time
         
         self.assertEqual(expected_direction, returned_direction)
         self.assertEqual(expected_pos, returned_position)
